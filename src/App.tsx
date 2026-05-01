1ef1e33ed22d5c65a8036efbbf67d2f9c4d64b04
Javier-Javi <benedictjavier6@gmail.com>
2026-05-01 15:30:45 +0800

    refactor: update App component to render Logs and remove DashboardPage; add SidebarNavigationSection for Logs page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import { LoginPage } from './pages/Login'
 import { DashboardPage } from './pages/Dashboard/Dashboard'
+import { Logs } from './pages/Logs'
 
 function App() {
-  return <DashboardPage />
+  return(
+    <Logs />
+  )
 }
 
 export default App
