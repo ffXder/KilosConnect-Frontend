c06412297a815b79b47eefcb97bbed8f2b92b7d1
ffXder <fajardofredderick@gmail.com>
2026-05-01 22:12:51 +0800

    refactor(App): streamline component structure and improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import DashboardPage from './pages/Dashboard/Dashboard'
 import { LoginPage } from './pages/Login'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to="/login" replace />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/dashboard' element={<DashboardPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" replace />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route 
+          path='/dashboard'
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  
   )
 }
 
