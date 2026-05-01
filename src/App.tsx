89a695d80e4c26e2a1074165105aa57bbd3a8d03
ffXder <fajardofredderick@gmail.com>
2026-05-01 16:53:00 +0800

    refactor App component to implement routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import DashboardPage from './pages/Dashboard/Dashboard'
 import { LoginPage } from './pages/Login'
-import { DashboardPage } from './pages/Dashboard/Dashboard'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
-  return <DashboardPage />
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to="/login" replace />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
+ 
