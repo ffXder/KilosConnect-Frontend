13b40b52560e49073c4113ce0614e62e81722003
Javier-Javi <benedictjavier6@gmail.com>
2026-05-05 05:05:42 +0800

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,67 @@
 import './App.css'
-import ManageProfileMain from '../RealFrontendOnly/ManageProfile/ManageProfileMain';
-import Logs from '../RealFrontendOnly/LogsModule/LogsMain';
-import ProfileMain from '../RealFrontendOnly/Profile/ProfileMain';
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { LoadingPage } from './pages/Loading'
+import { LoginPage } from './pages/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import  Logs  from './pages/LogsModule/LogsMain'
+import ManageProfileMain from './pages/ManageProfile/ManageProfileMain'
+import ProfileMain from './pages/Profile/ProfileMain'
+
+
 
 function App() {
+  const [isPageLoading, setIsPageLoading] = useState(true);
+
+  useEffect(() => {
+    const initApp = async () => {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 2000)); 
+      } finally {
+        setIsPageLoading(false);
+      }
+    };
+
+    initApp();
+  }, []);
+
+  if (isPageLoading) {
+    return <LoadingPage />;
+  }
+
   return (
-    <div className="min-h-screen bg-[#f0f2f5]">
-      <ProfileMain/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" replace />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/audit-logs' 
+        element={
+          <ProtectedRoute allowedRoles={['admin', 'custodian']}>
+              <Logs />
+            </ProtectedRoute>
+          }
+          />
+          <Route path='/profile'
+          element={
+            <ProtectedRoute allowedRoles={['admin', 'custodian']}>
+                <ProfileMain />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/manage-accounts'
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+              <ManageProfileMain />
+          </ProtectedRoute>
+        }
+        />
+      {/* redirects to login if not found or authenticated */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  
+  );
 }
 
 export default App
-  
+ 
