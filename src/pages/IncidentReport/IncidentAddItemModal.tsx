851e37815cb0f2f4458fc79d8709075d177c59a9
ffXder <fajardofredderick@gmail.com>
2026-05-07 19:25:02 +0800

    added option as urgent and critical

--- src/pages/IncidentReport/IncidentAddItemModal.tsx
+++ src/pages/IncidentReport/IncidentAddItemModal.tsx
@@ -100,6 +100,8 @@ const IncidentAddItemModal: React.FC<IncidentAddItemModalProps> = ({ isOpen, onC
                 <option value="Low">Low</option>
                 <option value="Medium">Medium</option>
                 <option value="High">High</option>
+                <option value="Urgent">Urgent</option>
+                <option value="Critical">Critical</option>
               </select>
               <ChevronDown className="absolute right-4 bottom-4 text-gray-400 pointer-events-none" size={18} />
             </div>
