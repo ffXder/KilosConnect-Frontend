6616edade6d75a6cb0ee2d7f15f1f2e364c3dfec
ffXder <fajardofredderick@gmail.com>
2026-05-07 19:25:26 +0800

    removed notif icon and profile icon

--- src/pages/ManageAccounts/AccountStatsSection.tsx
+++ src/pages/ManageAccounts/AccountStatsSection.tsx
@@ -8,10 +8,7 @@ export const AccountsStatsSection: React.FC = () => {
         <h1 className="font-semibold text-[36px] m-0">Manage Accounts</h1>
         <p className="text-[#6b6b6b] text-base">Add, edit, and manage user accounts</p>
       </div>
-      <div className="flex items-center gap-4">
-        <img className="w-10 h-10" src="https://c.animaapp.com/C3N4JJvt/img/notification@2x.png" alt="notifications" />
-        <img className="w-10 h-10 rounded-full" src="https://c.animaapp.com/C3N4JJvt/img/profile@2x.png" alt="profile" />
-      </div>
+      
     </header>
   );
 };
