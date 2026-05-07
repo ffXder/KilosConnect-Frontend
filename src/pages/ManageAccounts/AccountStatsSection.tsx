ec4d8be5ee69deb9b7a11496e71fdb122527ef53
ffXder <fajardofredderick@gmail.com>
2026-05-08 03:57:49 +0800

    change font and remove background header

--- src/pages/ManageAccounts/AccountStatsSection.tsx
+++ src/pages/ManageAccounts/AccountStatsSection.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export const AccountsStatsSection: React.FC = () => {
   return (
-    <header className="flex items-center justify-between px-8 pt-8 pb-4 bg-white border-b border-[#e8e8e8]">
+    <header className="flex items-center justify-between px-8 pt-8 pb-4">
       <div>
-        <h1 className="font-semibold text-[36px] m-0">Manage Accounts</h1>
+        <h1 className="[font-family:'Poppins',Helvetica] text-3xl font-bold text-gray-900 tracking-tigh">Manage Accounts</h1>
         <p className="text-[#6b6b6b] text-base">Add, edit, and manage user accounts</p>
       </div>
       
