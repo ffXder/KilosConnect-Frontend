dd49df862f5cb29f57d0dc56ee8b866ba2fb18d0
ffXder <fajardofredderick@gmail.com>
2026-07-17 01:24:14 +0800

    refactor: update Asset interface and related types for improved structure and clarity

--- src/types/asset.ts
+++ src/types/asset.ts
@@ -1,19 +1,24 @@
 export interface Asset {
     _id: string;
     assetId: string;
+    qrCode: string;
     name: string;
-    condition: "Good Condition" | "Needs Repair" | "Needs Replacement" | "Under Repair";
+    category: string;
+    condition: "Working" | "Needs Repair" | "Under Repair" | "Needs Replacement" | "Hazardous" | "Decommissioned";
     purchaseDate: string;
-    quantity: number;
+    srp: number;
+    cumulativeRepairCost: number;
     area: string;
+    baselineImageUrl?: string | null;
     description?: string;
     isArchived: boolean;
     archivedAt: string | null;
     archivedBy: string | null;
+   recommendation?: 'Replace Immediately' | 'Monitor Closely' | 'Healthy' | 'Unknown SRP, cannot determine replacement recommendation';
 }
 
 //create
-export type NewAsset = Omit<Asset, '_id' | 'assetId' | 'isArchived' | 'archivedAt' | 'archivedBy'>;
+export type NewAsset = Omit<Asset, '_id' | 'assetId' | 'qrCode' | 'cumulativeRepairCost' | 'isArchived' | 'archivedAt' | 'archivedBy' | 'recommendation'>;
 
 //update
-export type UpdateAsset = Partial<Omit<Asset, '_id' | 'assetId' | 'isArchived' | 'archivedAt' | 'archivedBy'>>;
+export type UpdateAsset = Partial<Omit<Asset, '_id' | 'assetId' | 'qrCode' | 'isArchived' | 'archivedAt' | 'archivedBy' | 'recommendation'>>;
