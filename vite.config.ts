a1dce0eaba082645d217666378cd6a4382159104
ffXder <fajardofredderick@gmail.com>
2026-07-22 20:33:30 +0800

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'; // for testing temporary HTTPS
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    basicSsl()
   ],
   server: {
+    host: true,
     proxy: {
       '/api' : {
         target: 'http://localhost:5000',
