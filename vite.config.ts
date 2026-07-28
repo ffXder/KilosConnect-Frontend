e7554f0f696d751fcc3a4fba147a3823fa3c0460
ffXder <fajardofredderick@gmail.com>
2026-07-28 21:41:35 +0800

    commented out basicSsl

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'; // for testing temporary HTTPS
+//import basicSsl from '@vitejs/plugin-basic-ssl'; // for testing temporary HTTPS
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    basicSsl()
+    // basicSsl()
   ],
   server: {
-    host: true,
+    // host: true,
     proxy: {
       '/api' : {
         target: 'http://localhost:5000',
