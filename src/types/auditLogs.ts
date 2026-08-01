cd470df702c31e51262f7190d7f8b578c8eff507
ffXder <fajardofredderick@gmail.com>
2026-08-02 02:19:31 +0800

    refactor: update AuditLogs types to use User interface and enhance pagination structure

--- src/types/auditLogs.ts
+++ src/types/auditLogs.ts
@@ -1,10 +1,32 @@
+// to get the populated users
+export interface User {
+  _id: string;
+  userId?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+}
+
 export interface AuditLogs {
   _id: string;
   action: 'CREATE' | 'UPDATE' | 'ARCHIVE' | 'UNARCHIVE' | 'DELETE' | 'CLAIM' | 'UNCLAIM' | 'VIEW' | 'COMPLETE' | 'MISSED';
   module: 'Asset' | 'Consumable' | 'Inventory' | 'LostAndFound' | 'IncidentReport' | 'Task';
   targetId: string | null;
-  performedBy: string | null;
+  performedBy: User;
   details: string | null;
   createdAt: string;         
   updatedAt: string;
+}
+
+export interface PaginationData {
+  currentPage: number;
+  totalPages: number;
+  totalLogs: number;
+}
+
+export interface AuditLogsApiResponse {
+  logs: AuditLogs[];
+  stats?: Record<string, number>
+  pagination: PaginationData;
 }
